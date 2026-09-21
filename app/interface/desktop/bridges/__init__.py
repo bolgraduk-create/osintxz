255e7e2ef4cb5f571df402f5a5ded52613b81da0
bolgraduk-create <bolgraduk@gmail.com>
2026-09-21 19:54:22 +0300

    R13.28b export AnalysisBridge

--- app/interface/desktop/bridges/__init__.py
+++ app/interface/desktop/bridges/__init__.py
@@ -1,11 +1,13 @@
 """Python view-models exposed to the Qt Quick presentation layer."""
 
+from app.interface.desktop.bridges.analysis_bridge import AnalysisBridge
 from app.interface.desktop.bridges.desktop_bridge import DesktopBridge
 from app.interface.desktop.bridges.investigation_search_bridge import InvestigationSearchBridge
 from app.interface.desktop.bridges.registry_center_bridge import RegistryCenterBridge
 from app.interface.desktop.bridges.source_center_bridge import SourceCenterBridge
 
 __all__ = [
+    "AnalysisBridge",
     "DesktopBridge",
     "InvestigationSearchBridge",
     "RegistryCenterBridge",
