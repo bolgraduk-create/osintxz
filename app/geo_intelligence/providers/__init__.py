0595c19b84629c9462649ffd8e94eeaa166545a1
bolgraduk-create <bolgraduk@gmail.com>
2026-09-24 20:47:46 +0300

    GEO-C1 add geo provider package

---
+++ app/geo_intelligence/providers/__init__.py
@@ -0,0 +1,7 @@
+from app.geo_intelligence.providers.open_meteo import OpenMeteoHistoricalProvider
+from app.geo_intelligence.providers.overpass import OverpassNearbyProvider
+
+__all__ = [
+    "OpenMeteoHistoricalProvider",
+    "OverpassNearbyProvider",
+]
