0e42ebfbbd346c1543dac6ab40142859ab6dd539
bolgraduk-create <bolgraduk@gmail.com>
2026-09-25 17:32:02 +0300

    GEO-C2 export Sentinel-2 catalogue provider

--- app/geo_intelligence/providers/__init__.py
+++ app/geo_intelligence/providers/__init__.py
@@ -1,7 +1,13 @@
+from app.geo_intelligence.providers.copernicus_sentinel2 import (
+    CopernicusSentinel2CatalogProvider,
+    Sentinel2SceneSearchRequest,
+)
 from app.geo_intelligence.providers.open_meteo import OpenMeteoHistoricalProvider
 from app.geo_intelligence.providers.overpass import OverpassNearbyProvider
 
 __all__ = [
+    "CopernicusSentinel2CatalogProvider",
+    "Sentinel2SceneSearchRequest",
     "OpenMeteoHistoricalProvider",
     "OverpassNearbyProvider",
 ]
